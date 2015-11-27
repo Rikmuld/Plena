5792e2256629fb2d51b1688cf03dc57d7325293c
= <info@rikmuld.me>
2015-11-27 11:21:20 +0100

    pong

--- Plena/Plena/public/tests/t6.ts
+++ Plena/Plena/public/tests/t6.ts
@@ -2,23 +2,26 @@
     var bat: Grix
     var ball: Grix;
 
-    var x1 = window.innerHeight / 2;
-    var x2 = window.innerHeight / 2;
-
-    var xB = window.innerWidth / 2;
-    var yB = window.innerHeight / 2;
+    var x1;
+    var x2;
+    var xB;
+    var yB;
     var v: number;
     var vm = 10;
     var direction: number;
     var radius = 20;
 
-    var isPlaying = false;
+    var isPlaying;
+    var score = 0;
 
     export function setup() {
         bat = new Grix().colorRGB(160, 160, 240).rect(20, 120).populate();
         ball = new Grix().circle(radius).colorRGB(240, 160, 160).populate();
 
         Keyboard.addPressedEvent(startBall, Keyboard.KEY_SPACE);
+
+        kill(false);
+        score = 0;
     }
 
     export function update(delta: number) {
@@ -37,7 +40,18 @@
             yB += v * Math.sin(direction);
 
             if (yB - radius <= 0 || yB + radius >= Plena.height) direction = Math.PI * 2 - direction;
+            if (xB - radius <= 0 || xB + radius >= Plena.width) kill(xB - radius <= 0);
         }
+    }
+
+    function kill(left: boolean) {
+        var xB = Plena.width / 2;
+        var yB = Plena.height / 2;
+        var x1 = Plena.height / 2;
+        var x2 = Plena.height / 2;
+
+        isPlaying = false;
+        score += 1;
     }
 
     export function render(delta: number) {
@@ -57,7 +71,7 @@
     function startBall() {
         if (!isPlaying) {
             v = 5;
-            direction = Math.random() * Math.PI * 2;
+            direction = Math.floor(Math.random()*2) * Math.PI;
             isPlaying = true;
         }
     }
